416af0289a3db0c92c503d2feb2e4be5931e38e4
Louise <taylor.louise.ann@gmail.com>
2018-06-29 14:46:24 +0100

    Add ESlint and nodemon

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express(); // Execute an instance of express
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
@@ -12,6 +13,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening at ${chalk.green(port)}`);
 });
