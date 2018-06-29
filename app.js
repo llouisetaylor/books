184d1eec815d9afd0ffdbd23a3ff4c6ed70bf158
Louise <taylor.louise.ann@gmail.com>
2018-06-29 14:04:23 +0100

    Serve HTML file

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express(); // Execute an instance of express
 
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Hello from my library app!');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, () => {
